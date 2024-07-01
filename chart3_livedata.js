78f21c906446414b74204e6e40dfc253ce9d2548
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-07-01 10:40:55 +0200

    debugging

--- chart3_livedata.js
+++ chart3_livedata.js
@@ -1,13 +1,17 @@
-// uses the fetch API to get data from the external JSON source. 
-//The data is then mapped to the format required by Chart.js.
+// The fetchData function retrieves data from the external JSON source and
+// maps it to the format required by Chart.js.
+
 async function fetchData() {
     const response = await fetch("https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=10&length=10&type=json");
     const data = await response.json();
     return data.map(point => ({ x: point[0], y: parseInt(point[1]) }));
 }
 
+//The createChart function initializes the Chart.js chart with the fetched data points.
+//The chart options include the x-axis set to ‘linear’ and the y-axis starting from zero.
+
 async function createChart() {
-    const ctx = document.getElementById('chartContainer').getContext('2d');
+    const ctx = document.getElementById('chart3').getContext('2d');
     const dataPoints = await fetchData();
     
     const chart = new Chart(ctx, {
